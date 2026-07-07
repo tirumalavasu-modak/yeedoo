95d69a962cfbe89434749a86444624708775f413
tirumalavasu-modak <78591878+tirumalavasu-modak@users.noreply.github.com>
2026-07-07 14:27:15 +0530

    Apply CSS and images from Webflow export

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "normalize.css";
 import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
+import "../../public/styles/webflow.css";
+import "../../public/styles/lp-yeedu.webflow.css";
 import "../../public/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
